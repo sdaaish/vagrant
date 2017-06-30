8d82429a7f0ce7d2bcbe49783d8a3b796de0903c
Stig Dahl <stig@charlottendal.net>
2017-06-30 13:59:16 +0200

    Added comments.

--- win10/winrm-set.cmd
+++ win10/winrm-set.cmd
@@ -1,5 +1,26 @@
-winrm quickconfig -q
-winrm set winrm/config/winrs @{MaxMemoryPerShellMB="300"}
-winrm set winrm/config @{MaxTimeoutms="1800000"}
-winrm set winrm/config/service @{AllowUnencrypted="true"}
-winrm set winrm/config/service/auth @{Basic="true"}
+@echo off
+
+:: Sets up WINRM so RDP works for vagrant.
+:: From https://dennypc.wordpress.com/2014/06/09/creating-a-windows-box-with-vagrant-1-6/
+::
+
+echo Administrative permissions required. Detecting permissions...
+echo.
+
+net session >nul 2>&1
+
+if %errorLevel% == 0 (
+   winrm quickconfig -q
+   winrm set winrm/config/winrs @{MaxMemoryPerShellMB="300"}
+   winrm set winrm/config @{MaxTimeoutms="1800000"}
+   winrm set winrm/config/service @{AllowUnencrypted="true"}
+   winrm set winrm/config/service/auth @{Basic="true"}
+   winrm set winrm/config/client/auth @{Basic="true"}
+
+   netsh advfirewall firewall add rule name="WinRM-HTTP" dir=in localport=5985 protocol=TCP action=allow
+)
+else (
+  echo Failure: You must run this batch file as Administrator.
+)
+
+pause
